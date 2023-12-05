ee59a4e10211cb57d17b711aef08d00c078838d4
Saumya Mehta <saumyakmehta26@gmail.com>
2023-12-05 19:05:32 +0530

    third commit

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,46 +1,46 @@
-// import { createContext, useState } from "react";
-
-// const AuthContext = createContext({});
-
-// export const AuthProvider = ({ children }) => {
-//     const [auth, setAuth] = useState({});
-
-//     return (
-//         <AuthContext.Provider value={{auth, setAuth}}>
-//             {children}
-//         </AuthContext.Provider>
-//     )
-// }
-
-// export default AuthContext;
-
 import { createContext, useState } from "react";
 
 const AuthContext = createContext({});
 
-const AuthProvider = ({ children })=> {
-    const [token, setToken] = useState(null);
-    const [username, setUsername] = useState(null);
-    const [loginSuccess, setLoginSuccess] = useState(false);
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState({});
 
-    const login = (token, username) => {
-        console.log('Welcome -> ', username);
-        setToken(token);
-        setUsername(username);
-        setLoginSuccess(true);
-    };
-
-    const logout = () => {
-        setToken(null);
-        setUsername(null);
-        setLoginSuccess(false);
-    };
-
-return(
-        <AuthContext.Provider value={{token, username, loginSuccess, login, logout}}>
+    return (
+        <AuthContext.Provider value={{auth, setAuth}}>
             {children}
         </AuthContext.Provider>
-    );
-};
+    )
+}
 
-export {AuthContext, AuthProvider};
+export default AuthContext;
+
+// import { createContext, useState } from "react";
+
+// const AuthContext = createContext({});
+
+// const AuthProvider = ({ children })=> {
+//     const [token, setToken] = useState(null);
+//     const [username, setUsername] = useState(null);
+//     const [loginSuccess, setLoginSuccess] = useState(false);
+
+//     const login = (token, username) => {
+//         console.log('Welcome -> ', username);
+//         setToken(token);
+//         setUsername(username);
+//         setLoginSuccess(true);
+//     };
+
+//     const logout = () => {
+//         setToken(null);
+//         setUsername(null);
+//         setLoginSuccess(false);
+//     };
+
+// return(
+//         <AuthContext.Provider value={{token, username, loginSuccess, login, logout}}>
+//             {children}
+//         </AuthContext.Provider>
+//     );
+// };
+
+// export {AuthContext, AuthProvider};
